9782167cc303b89960eea7b8ad489cf78b7fbf8b
hemant1456 <hkbhambhu@gmail.com>
2025-12-31 13:16:54 +0530

    made num_class input variable

--- models/cifar10_resnet_18.py
+++ models/cifar10_resnet_18.py
@@ -8,18 +8,17 @@ class basic_block1(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=in_channels*2,kernel_size=3, padding=1, stride = 2,bias=False)
         self.bn1 = nn.BatchNorm2d(in_channels*2)
         self.relu1 = nn.ReLU()
-        self.drop1 = nn.Dropout(0.1)
 
         self.conv2 = nn.Conv2d(in_channels=in_channels*2, out_channels=in_channels*2,kernel_size=3, padding=1, stride = 1,bias=False)
         self.bn2 = nn.BatchNorm2d(in_channels*2)
         self.relu2 = nn.ReLU()
-        self.drop2 = nn.Dropout(0.1)
+        self.drop = nn.Dropout2d(0.1)
 
         self.downsample = nn.Conv2d(in_channels=in_channels, out_channels=in_channels*2,kernel_size=1, padding=0, stride = 2,bias=False)
     def forward(self,x):
         identity = self.downsample(x)
-        x = self.drop1(self.relu1(self.bn1(self.conv1(x))))
-        x = self.drop2(self.relu2(identity + self.bn2(self.conv2(x))))
+        x = self.relu1(self.bn1(self.conv1(x)))
+        x = self.drop(self.relu2(identity + self.bn2(self.conv2(x))))
         return x
 
 class basic_block2(nn.Module):
@@ -28,17 +27,16 @@ class basic_block2(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=in_channels,kernel_size=3, padding=1, stride = 1,bias=False)
         self.bn1 = nn.BatchNorm2d(in_channels)
         self.relu1 = nn.ReLU()
-        self.drop1 = nn.Dropout(0.1)
 
         self.conv2 = nn.Conv2d(in_channels=in_channels, out_channels=in_channels,kernel_size=3, padding=1, stride = 1,bias=False)
         self.bn2 = nn.BatchNorm2d(in_channels)
         self.relu2 = nn.ReLU()
-        self.drop2 = nn.Dropout(0.1)
+        self.drop = nn.Dropout2d(0.1)
 
     def forward(self,x):
         identity = x
-        x = self.drop1(self.relu1(self.bn1(self.conv1(x))))
-        x = self.drop2(self.relu2(identity + self.bn2(self.conv2(x))))
+        x = self.relu1(self.bn1(self.conv1(x)))
+        x = self.drop(self.relu2(identity + self.bn2(self.conv2(x))))
         return x
 
 
@@ -58,7 +56,7 @@ class resnet_block(nn.Module):
 
 
 class resnet18(nn.Module):
-    def __init__(self):
+    def __init__(self,n_classes=10):
         super().__init__()
         self.conv1 = nn.Conv2d(3,32,3,1,1) # 32x32x32
         self.bn1 = nn.BatchNorm2d(32)
@@ -72,7 +70,7 @@ class resnet18(nn.Module):
 
         self.gap = nn.AdaptiveAvgPool2d((1,1))
 
-        self.conv2 = nn.Conv2d(512,10,1,1,0)
+        self.conv2 = nn.Conv2d(512,n_classes,1,1,0)
 
     def forward(self,x):
         x = self.relu1(self.bn1(self.conv1(x)))
