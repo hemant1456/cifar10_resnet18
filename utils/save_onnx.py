36a0ddc6938a941b4ab19ac9753b47961adad17e
hemant1456 <hkbhambhu@gmail.com>
2026-01-02 16:26:51 +0530

    changed input shape to 64,64 and classes to correct number

--- utils/save_onnx.py
+++ utils/save_onnx.py
@@ -1,9 +1,9 @@
 import torch
-checkpoint = torch.load('./checkpoints/model_final.pt',map_location=torch.device('cpu'))
+checkpoint = torch.load('./final_model.pt',map_location=torch.device('cpu'))
 
 from models.cifar10_resnet_18 import resnet18
 
-model = resnet18()
+model = resnet18(n_classes=200)
 
 model.load_state_dict(checkpoint['model_params'])
 
@@ -11,8 +11,8 @@ model.load_state_dict(checkpoint['model_params'])
 from onnxruntime.quantization import quantize_dynamic, QuantType
 onnx_program = torch.onnx.export(
     model = model,
-    args = torch.randn(1,3,32,32),
-    f = 'final_model_aws.onnx',
+    args = torch.randn(1,3,64,64),
+    f = 'final_model_imagenet.onnx',
     input_names = ['input'],
     output_names= ['output'],
     dynamic_axes={
